9db03b3ed0396f3def8080ad585e1a4f6f9e8e97
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-09-04 10:42:35 +0530

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,89 @@
+class Presets(object):
+    HELP_TEXT = """How to Use me..?
+    
+• <b>Send Me A YouTube Link Or Search in Inline mode to gt Link
+</b>
+• <b>Send Me A Image To Set Thumbnail </b>
+
+• <b>To Delete Thumbnail choose desired button</b>
+    """
+    WELCOME_MSG = """<b>Hai {}\na Simple YouTube Downloader Bot that can:</b>
+  ➠ Download YouTube videos
+  ➠ Download audio from YouTube videos
+  
+  ➠ Support YouTube Shorts Link
+  ➠ Support inline mode
+  
+  
+NB: <code> Bot may slow ,so wait 5 - 10 second to bot respond....
+please dont spam with links...</code>
+┈┈┈••💙✿❤✿💚••┈┈┈
+    """
+    OPTIONS_TXT = "<b>hey, whatsup\ndont be shy \n\n ➠ Send Me A YouTube Link or Search inline Now</b>"
+    RESULTS_TXT = "👀 Results:"
+    NO_RESULTS = "❌  No Results Found: "
+    DESCRIPTION = "Duration: {} || {}"
+    NOT_AUTH_TXT = "❌ ❌ 𝐘𝐨𝐮 𝐚𝐫𝐞 𝐧𝐨𝐭 𝐚𝐮𝐭𝐡𝐨𝐫𝐢𝐳𝐞𝐝 ❌ ❌"
+    DEFAULT_TITLE = "Join Our Update Channel"
+    DEFAULT_THUMB_URL = "https://image.flaticon.com/icons/png/512/25/25231.png"
+    DEFAULT_LINK = "https://telegram.me/MyTestBotZ"
+    DEFAULT_DESCRIPTION = "Simple YouTube downloader bot with inline mode support"
+    DEV_TITLE = "Bot Creater"
+    DEV_THUMB_URL = "https://freepikpsd.com/media/2019/10/software-developer-icon-png-2-Transparent-Images.png"
+    DEV_LINK = "https://t.me/OO7RoBot"
+    DEV_DESCRIPTION = "𝐍𝐚𝐦𝐞: 𝐑𝐞𝐧𝐣𝐢𝐭𝐡 𝐌𝐚𝐧𝐠𝐚𝐥 | 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦"
+    SHARE_BUTTON_TEXT = "Hia 👋\n i found an awesome bot for download YouTube videos, audio into telegram with inline mod support\n\n Bot : @{username}\n"
+    SAVED_THUMB = "<b>✅ Thumbnail Saved Successfully</b>\n<code>This file will be used in next YouTube " \
+                  "downloads until you clear it !</code> "
+    WAIT_MESSAGE = "Please wait for a second"
+    THUMB_CAPTION = "<code>This image is your current thumbnail, Tap </code><b> DEL THUMB </b><code> if you wish to " \
+                    "clear it !</code> "
+    NO_THUMB = "No Thumbnail Found !"
+    DEL_THUMB_CNF = "Thumbnail Cleared Successfully ✅"
+    LINK_ERROR = "𝐒𝐨𝐦𝐞 𝐞𝐫𝐫𝐨𝐫𝐬 𝐨𝐜𝐜𝐮𝐫𝐫𝐞𝐝 𝐰𝐡𝐢𝐥𝐞 𝐭𝐡𝐞 𝐩𝐫𝐨𝐜𝐞𝐬𝐬 !\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐭𝐫𝐲 𝐚𝐠𝐚𝐢𝐧 𝐥𝐚𝐭𝐞𝐫.."
+    #
+    #
+    #
+    NO_VOID_FORMAT_FOUND = "<code>{}</code>"
+    FINISHED_PROGRESS_STR = "▰"
+    UN_FINISHED_PROGRESS_STR = "▱"
+    CHECKING_LINK = " AnalyZing your Link.. ⏳"
+    DOWNLOAD_START = """<b>Downloading to my server 📥</b>
+   
+ <code>Please wait...⏳ 🙇🙇🙇
+  it takes time depend on File Size</code>"""
+    UPLOAD_START = """<b>File Download Successfully 😊</b>
+    <code>Now Uploading to Telegram 📤</code>"""
+    NOT_DOWNLOADABLE = "URL not Downloadable !"
+    CANCEL_PROCESS = "<b>Process Cancelled Successfully</b>  ✅"
+    SEND_TEXT = "<b>Processing...</b>\n<i>This message will automatically disappear when the broadcasting is finished</i> "
+    REPLY_ERROR = "<i>Use this command as a replay to any telegram message with out any spaces.</i>"
+    USERS_LIST = "<b>Total: {}</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
+    WAIT_MSG = "<b>Processing...</b>\n<i>This will take some time...</i>"
+    PROMPT_THUMB = "<b>Do you want to set this image as a thumbnail?</b>"
+    FORMAT_SELECTION = """
+<b>Title -</b> {}
+<b>Channel -</b> <a href={}>{}</a>
+<b>Uploaded On -</b> {}
+<b>Views -</b> {}  |  <b>Rating:</b> {}
+
+<b>Select the desired format:</b>
+🎞  - Stream format (left side)
+📁  - File format (right side)
+
+    """
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | newfilename | username | password"""
+    CUSTOM_CAPTION_UL_FILE = "\xad \xad\n<code>{}</code>\n\n<b><a href='https://t.me/MyTestBotZ'>Uploaded by</a>: @TG_YTbot</b>"
+    RCHD_TG_API_LIMIT = "Detected File Size: {}\nSorry. But, I cannot upload files " \
+                        "greater than 1.95GB due to Telegram API limitations."
+    AD_STRING_TO_REPLACE = "please report this issue on https://yt-dl.org/bug . Make sure you are using the " \
+                           "latest version; see  https://yt-dl.org/update  on how to update. Be sure to call " \
+                           "youtube-dl with the --verbose flag and include its complete output."
+    BLOCK_LIST_TEXT = "This url is not an YouTube Link ,so I can not upload this URL.\n\n➠ Use @AnyURLbot or @AnyURLDLBot"
+    REPORT_SITE_TEXT = "<code>Sorry not uploading in this site here because this site is reporting site.</code>"
+    SOMETHING_WRONG = "<code>Something Wrong. Try again.</code>"
+    FORCE_SUBSCRIBE_TEXT = """<b>You must Join Updates Channel to use me😌.</b>
+    
+    <i>Due to Overload, Only Channel Subscribers can use this Bot!🤷</i>"""
+    BANNED_USER_TEXT = "<code>You are Banned from Using our Service!</code>"
